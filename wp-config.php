af1a0a7156d822e09101a723cf675cd74c461ae6
iutin <evgeniy.iutin@gmail.com>
2016-02-16 14:22:09 +0200

    shop start

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** Параметры MySQL: Эту информацию можно получить у вашего хостинг-провайдера ** //
 /** Имя базы данных для WordPress */
-define('DB_NAME', 'stolkomp_db');
+define('DB_NAME', 'stoliki_shop');
 
 /** Имя пользователя MySQL */
-define('DB_USER', 'iutin');
+define('DB_USER', 'root');
 
 /** Пароль к базе данных MySQL */
-define('DB_PASSWORD', 'webares_team');
+define('DB_PASSWORD', 'root');
 
 /** Имя сервера MySQL */
-define('DB_HOST', '134.249.128.40');
+define('DB_HOST', 'localhost');
 
 /** Кодировка базы данных для создания таблиц. */
 define('DB_CHARSET', 'utf8');
