23934961271a09a49b819a21a391a5fdcf0084bb
iutin <evgeniy.iutin@gmail.com>
2016-02-16 17:00:27 +0200

    set themes

---
+++ wp-content/themes/stolkomp/page-home.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: iutin
+ * Date: 16.02.16
+ * Time: 15:59
+ */
+/**
+ * Template Name: Home Page
+ */
+?>
+<?php get_header(); ?>
+
+<?php the_content(); ?>
+
+<?php get_footer(); ?>
---
+++ wp-content/themes/stolkomp/templates/header.php
@@ -0,0 +1,58 @@
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+                <div class="register-block">
+                    <a href="#">Log In</a>
+                    <span>or</span>
+                    <a href="#">Register</a>
+                </div>
+            </div>
+
+        </div>
+        <div class="row">
+            <div class="col-sm-12">
+                <h1 class='brand'><a href="<?php echo get_site_url(); ?>">Stolcomp</a></h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="wrapper-block">
+                    <div class="row">
+                        <div class="col-sm-10">
+                            <nav role='main-nav'>
+                                <ul class='main-nav'>
+                                    <li class='active'>
+                                        <a href="index.html">Home</a>
+                                    </li>
+
+                                    <li>
+                                        <a href="category-grid.html" class="has-dropdown">Category</a>
+                                        <ul>
+                                            <li><a href="category-grid.html">Category grid</a></li>
+                                            <li><a href="category-list.html">Category-list</a></li>
+                                        </ul>
+                                    </li>
+
+                                    <li><a href="checkout.html">Checkout</a></li>
+                                    <li>
+                                        <a href="product.html">Product</a>
+                                    </li>
+                                    <li><a href="shopping-cart.html">Cart</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                        <div class="col-sm-2">
+                            <div class="input-group search-block">
+                                <input type="text" class="form-control" placeholder="Search here">
+                                    <span class="input-group-btn">
+                                      <button class="btn btn-default" type="button"><span class='glyphicon glyphicon-search'></span></button>
+                                    </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
