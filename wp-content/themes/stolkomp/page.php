e75127c13eb24413bbec2d76812b05ca08d7c4b9
iutin <evgeniy.iutin@gmail.com>
2016-02-17 10:37:24 +0200

    set themes

--- wp-content/themes/stolkomp/page.php
+++ wp-content/themes/stolkomp/page.php
@@ -1,16 +1,16 @@
 <?php
 get_header(); ?>
+<section id='main'>
+    <div class="container">
+        <div class="row">
+            <?php while ( have_posts() ) : the_post(); ?>
 
-<div id="primary" class="content-area">
-    <main id="main" class="site-main" role="main">
+                <?php the_content();?>
 
-        <?php while ( have_posts() ) : the_post(); ?>
+            <?php endwhile; // end of the loop. ?>
+        </div>
+    </div>
+</section>
 
-       <?php the_content();?>
-
-        <?php endwhile; // end of the loop. ?>
-
-    </main><!-- #main -->
-</div><!-- #primary -->
 <?php get_footer();
 ?>
